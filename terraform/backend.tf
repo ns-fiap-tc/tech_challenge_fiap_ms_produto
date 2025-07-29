01f0e6c80bfcae4d7961825249a63a9ce31a12f7
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-07-29 10:23:15 -0300

    fix: update S3 bucket name in Terraform backend configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
-# # Remote backend instance, to save tfstate
-# terraform {
-#   backend "s3" {
-#     bucket         = "lanchonete-tfstate-bucket"
-#     key            = "tech-challenge-app/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-lock"
-#     encrypt        = true
-#   }
-# }
+# Remote backend instance, to save tfstate
+terraform {
+  backend "s3" {
+    bucket         = "tc-lanchonete-tfstate-bucket"
+    key            = "tech-challenge-app/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
